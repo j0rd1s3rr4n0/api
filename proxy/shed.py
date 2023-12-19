a39b918588bcfb82db2d8547d7f5b540fd21363c
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-12-19 12:51:45 +0100

    Update Cron 5 min

--- proxy/shed.py
+++ proxy/shed.py
@@ -6,8 +6,9 @@ def ejecutar_main():
     subprocess.run(["python", "getProxy.py"])
 
 # Programar la ejecución de main.py cada 2 minutos
-schedule.every(1).minutes.do(ejecutar_main)
-
+schedule.every(5).minutes.do(ejecutar_main)
+# First run
+ejecutar_main()
 while True:
     schedule.run_pending()
     time.sleep(1)

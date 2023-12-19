e2e7a6dfc24ba250ff6602af8e9259eddf557605
Jordi Serrano <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2023-12-19 12:50:19 +0100

    Revert "Update: Cron 5 min"
    This reverts commit 48fb3b846a43e721835db337045136175fb74130.

--- proxy/shed.py
+++ proxy/shed.py
@@ -1,20 +1,13 @@
 import schedule
 import time
 import subprocess
-import os
-
-contador = 0
 
 def ejecutar_main():
-    global contador
-    contador+=1
-    os.system('title Contador: '+str(contador))
     subprocess.run(["python", "getProxy.py"])
 
 # Programar la ejecución de main.py cada 2 minutos
-schedule.every(5).minutes.do(ejecutar_main)
-ejecutar_main()
+schedule.every(1).minutes.do(ejecutar_main)
+
 while True:
-    time.sleep(1)
     schedule.run_pending()
     time.sleep(1)

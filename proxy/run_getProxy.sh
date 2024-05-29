5d881bfceb37c83dea5bd1007e11257cbb567c28
j0rd1s3rr4n0 <44474715+j0rd1s3rr4n0@users.noreply.github.com>
2024-05-29 21:23:37 +0000

    Test

--- proxy/run_getProxy.sh
+++ proxy/run_getProxy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Ruta al archivo Python
-python_script="getProxy.py"
+python_script="/var/api/proxy/getProxy.py"
+
+# Ruta del archivo de log para errores
+error_log="/var/api/proxy/errors.log"
+
+# Establecer el directorio de trabajo a /var/api/proxy
+cd /var/api/proxy
 
 # Verificar si el archivo Python existe
 if [ -f "$python_script" ]; then
-    # Ejecutar el script Python
-    python "$python_script"
+    # Ejecutar el script Python y redirigir errores
+    python3 "$python_script" 2>> "$error_log"
 else
-    echo "Error: El archivo $python_script no existe."
+    echo "Error: El archivo $python_script no existe." >> "$error_log"
 fi
-
